1ddbfc09501f3fec3da189de19f97f0c0d911043
Diran Li <diran.d.li@gmail.com>
2021-05-21 09:27:28 -0400

    Sample task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,8 +5,9 @@ import "@nomiclabs/hardhat-etherscan";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  console.log("Accounts:");
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
